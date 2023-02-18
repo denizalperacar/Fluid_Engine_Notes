eb030e44cf01e8b50f13339113b345808fa2ffee
Deniz A. Acar <59959106+denizalperacar@users.noreply.github.com>
2023-02-18 21:29:26 +0300

    updating visual studio

---
+++ Fluid_engine/Fluid_engine.cpp
@@ -0,0 +1,12 @@
+﻿// Fluid_engine.cpp : Defines the entry point for the application.
+//
+
+#include "Fluid_engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
