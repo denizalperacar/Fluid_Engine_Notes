cdf61d600fa4c6ab00848a68e0615c41ed1b01bd
denizalperacar <https://github.com/denizalperacar>
2023-03-09 01:14:50 +0300

    testing libtorch

---
+++ Fluid_engine/examples/test_tensor/configure_tensor.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include "torch/torch.h"
+
+
+int main () {
+
+  // tensor configuration
+  c10::TensorOptions options = torch::TensorOptions()
+    .dtype(torch::kFloat32)
+    .layout(torch::kStrided)
+    .device(torch::kCPU)
+    .requires_grad(true);
+
+  torch::Tensor tensor = torch::randn({100,3}, options);
+  std::cout << tensor * 2.0 + tensor << std::endl;
+
+  // at::parallel_for
+  // do the ray cutting the 3D voxel area.
+  
+
+}
